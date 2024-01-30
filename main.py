7e8f587cb565b52a7221fd9b5d70e6a78bb20254
Alex Post <postalex11@icloud.com>
2024-01-30 17:22:09 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import re
+
+dictionary = ["Apple", "Helpy", "Bagel", "Phone", "Siren", "Octal", "Phaze", "Bugel", "Zooty"]
+
+#potential letters for each spot
+potentialLetters = input("Enter potential letters: ")
+
+#create regex for our letter
+anyPattern = "["
+for letter in potentialLetters:
+    anyPattern += letter.upper()
+     
+anyPattern += "]"
+
+pattern = "" #overall regex pattern
+#construct the overall pattern
+for i in range(5):
+    userInput = input("Enter the letter in position " + str(i) + ", or use * for any of the potential letters: ")
+    if(len(userInput) != 1):
+        print("Please enter only one character.")
+    else:
+        if(userInput == "*"):
+            pattern += anyPattern
+        else:
+            pattern += userInput.upper()
+
+print("Pattern is: " + pattern)
+
+#loop over the words in dictionary to see if there is a match
+for words in dictionary:
+    if re.match(pattern, words.upper()):
+        print(words)
+
